9b3c8047b4110c2e80c839b1f1bcf41af0b22cda
actulurus <actulurus@gmail.com>
2023-10-30 16:53:30 +0100

    fix

--- static/clicker.js
+++ static/clicker.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ action: 'add_click' }),
+                    // body: JSON.stringify({ action: 'add_click' }),
                 });
 
             });
