c111c094ab792bca367c7078706ec69a455ea5fd
actulurus <actulurus@gmail.com>
2023-11-06 14:29:36 +0100

    test

--- app.py
+++ app.py
@@ -164,9 +164,9 @@ def test():
 @app.before_request
 def redirect_to_non_www():
     urlparts = urlparse(request.url)
-    if urlparts.netloc == 'www.'+app.config['SERVER_NAME']:
+    if urlparts.netloc == ('www.' + app.config['SERVER_NAME'] or "cernyrob.in"):
         urlparts_list = list(urlparts)
-        urlparts_list[1] = app.config['SERVER_NAME']
+        urlparts_list[1] = app.config['SERVER_NAME'] or "cernyrob.in"
         return redirect(urlunparse(urlparts_list), code=301)
 
 if __name__ == '__main__':
