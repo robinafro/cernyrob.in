936028c12e9ba148b4ce3e21e1ba433c3fa092e2
actulurus <actulurus@gmail.com>
2023-11-21 12:18:09 +0100

    deleted everything

--- cernyrobin_django/home/urls.py~
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path('', views.home, name="root"),
-        path('home/', views.home, name="home"),
-]
--- cernyrobin_django/home/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-def home(request):
-    return HttpResponse(loader.get_template('home.html').render())
