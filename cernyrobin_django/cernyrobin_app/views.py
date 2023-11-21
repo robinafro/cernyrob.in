2385c49e7fc37cd7995098cb210ddedda6094795
actulurus <actulurus@gmail.com>
2023-11-21 13:29:45 +0100

    fix views.py

--- cernyrobin_django/cernyrobin_app/views.py
+++ cernyrobin_django/cernyrobin_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from django.templates import loader
+from django.template import loader
 
 def home(request):
     return HttpResponse(loader.get_template("cernyrobin/home.html").render())
