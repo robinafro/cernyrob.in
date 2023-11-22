be635f715a1b9880430cbf54a646fdd843764555
actulurus <actulurus@gmail.com>
2023-11-22 08:37:37 +0100

    improved login

--- cernyrobin_django/cernyrobin_app/views.py
+++ cernyrobin_django/cernyrobin_app/views.py
@@ -5,11 +5,15 @@ from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-""" from .models import Room, Topic
-from .forms import RoomForm """
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+
+# Utils
+def go_back(request):
+    next_url = request.POST.get('next', request.GET.get('next', ''))
+
+    return redirect(next_url) if next_url else HttpResponse("200 OK")
 
 # Pages
 
@@ -23,44 +27,47 @@ def clicker(request):
     return render(request, "cernyrobin/clicker.html", context)
    
 def login_page(request):
-    if request.user.is_authenticated:
-        return redirect("home") #!
-    else:
-        context = {
-            "page": "login",
-        }
+    context = {
+        "page": "login",
+    }
 
-        return render(request, "cernyrobin/login.html", context)
+    return render(request, "cernyrobin/login.html", context)
+
+# API
 
 def login_submit(request):
-    if request.user.is_authenticated:
-        return HttpResponse("400 Bad Request")
-
     if request.method == "POST":
-        username = request.POST.get("username").lower()
+        username = request.POST.get("username")
         password = request.POST.get("password")
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(username=username.lower())
         except User.DoesNotExist:
+            if request.user.is_authenticated:
+                logout(request)
+
             user = User.objects.create_user(username=username, password=password)
             user.save()
-            return HttpResponse("200 OK")
+
+            login(request, user)
+
+            return go_back(request)
+
+        if user is not None:
+            return HttpResponse("409 Conflict")
 
         if user.check_password(password):
             user = authenticate(request, username=username, password=password)
             
             if user is not None:
                 login(request, user)
-                return HttpResponse("200 OK")
+                return go_back(request)
             else:
                 return HttpResponse("401 Unauthorized")
         else:
             return HttpResponse("401 Unauthorized")
 
     return HttpResponse("405 Method Not Allowed")
-        
-# Clicker
 
 def add_click(request):
     if request.method == "POST":
