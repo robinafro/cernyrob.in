c5244a7dda6a1f205d455cf6f17a69ed55cf5b7e
Actulurus <actulurus@gmail.com>
2023-11-23 16:35:08 +0100

    fix

--- cernyrobin_django/cernyrobin_app/clicker.py
+++ cernyrobin_django/cernyrobin_app/clicker.py
@@ -2,7 +2,7 @@ import cernyrobin_app.get_profile
 
 def add_click(request):
     user_profile = get_profile(request.user)
-    
+
     user_profile.clicks += user_profile.clicks_mult
 
     user_profile.save()
