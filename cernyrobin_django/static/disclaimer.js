77895bfaf4d49b12887078b036ae00b65094cea8
StuckInVim-dev <borecnik@protonmail.com>
2023-12-04 11:50:39 +0100

    add disc.js

---
+++ cernyrobin_django/static/disclaimer.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+document.getElementById('disclaimer-button').addEventListener('click', function() {
+    document.getElementById('disclaimer-footer').style.visibility = 'hidden';
+})})
