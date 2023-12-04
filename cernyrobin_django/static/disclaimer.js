6e2eb06842c20241971176a25837483b8135d6ba
StuckInVim-dev <borecnik@protonmail.com>
2023-12-04 12:54:19 +0100

    Added hideable disclaimer cookie

--- cernyrobin_django/static/disclaimer.js
+++ cernyrobin_django/static/disclaimer.js
@@ -1,5 +1,39 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  
-document.getElementById('disclaimer-button').addEventListener('click', function() {
-    document.getElementById('disclaimer-footer').style.visibility = 'hidden';
+
+    if(getCookie("hideDisclaimer") === "true"){
+        document.getElementById('disclaimer-footer').style.visibility = 'hidden';
+        console.log("hidden auto")
+    }
+
+    document.getElementById('disclaimer-button').addEventListener('click', function() {
+
+        document.getElementById('disclaimer-footer').style.visibility = 'hidden';
+        setCookie("hideDisclaimer", "true", 3); 
+        console.log("hidden man")
+
 })})
+
+
+
