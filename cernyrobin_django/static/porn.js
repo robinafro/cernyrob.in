6ac2ff161600c43bb246c029954edf5b4b4d5e95
Vítek Adam <stuckinvim@protonmail.com>
2024-01-08 23:58:49 +0100

    Edited popup style

--- cernyrobin_django/static/porn.js
+++ cernyrobin_django/static/porn.js
@@ -1,8 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-        document.getElementById("info").style.display = "block";
+        document.getElementById("info").style.display = "flex";
         console.log("displaying popup");
-    }, 1000);
+    }, 500);
+
+
+    var div = document.getElementById('info');
+var isLarge = false; // State tracking variable
+
+function toggleSize() {
+  // Change the size of the div based on its current state
+  div.style.width = isLarge ? '30%' : '35%';
+/*   div.style.height = isLarge ? '200px' : '300px'; */
+
+  isLarge = !isLarge; // Toggle state
+}
+
+//setInterval(toggleSize, 100); // Adjust the size every 1 second
+
+setInterval(toggleSize, 1000);
+
 });
 
 function closePopup() {
