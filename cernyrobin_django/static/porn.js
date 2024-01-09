f6df177cfb34f755f7b9c1798658b65469893b78
Vítek Adam <stuckinvim@protonmail.com>
2024-01-09 08:19:52 +0100

    Added buttons to popup

--- cernyrobin_django/static/porn.js
+++ cernyrobin_django/static/porn.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById("info").style.display = "flex";
-        console.log("displaying popup");
+
+        document.getElementById("rButton").style.visibility = "visible";
     }, 500);
 
 
@@ -10,15 +11,15 @@ var isLarge = false; // State tracking variable
 
 function toggleSize() {
   // Change the size of the div based on its current state
-  div.style.width = isLarge ? '30%' : '35%';
+  div.style.width = isLarge ? '35%' : '40%';
 /*   div.style.height = isLarge ? '200px' : '300px'; */
 
   isLarge = !isLarge; // Toggle state
 }
 
-//setInterval(toggleSize, 100); // Adjust the size every 1 second
+setInterval(toggleSize, 100); // Adjust the size every 1 second
 
-setInterval(toggleSize, 1000);
+//setInterval(toggleSize, 1000);
 
 });
 
