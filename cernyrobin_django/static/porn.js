9cde9b5a2359ede419f2b46fe020a5e8f4c93315
Actulurus <actulurus@gmail.com>
2024-01-14 16:14:11 +0100

    10% chance to appear

--- cernyrobin_django/static/porn.js
+++ cernyrobin_django/static/porn.js
@@ -1,47 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  if (Math.floor(Math.random() * 100) <= 10) {
+    setTimeout(function () {
+      document.getElementById("info").style.display = "flex";
 
-
-
-    setTimeout(function() {
-        document.getElementById("info").style.display = "flex";
-
-        document.getElementById("rButton").style.visibility = "visible";
-        document.getElementById("rButtonCont").style.visibility = "visible";
-    }, 500);
-
+      document.getElementById("rButton").style.visibility = "visible";
+      document.getElementById("rButtonCont").style.visibility = "visible";
+    }, Math.floor(Math.random() * 1000 + 500)); // Random time
 
     var div = document.getElementById('info');
-var isLarge = false; // State tracking variable
+    var isLarge = false; // State tracking variable
 
-function toggleSize() {
-  // Change the size of the div based on its current state
-  div.style.width = isLarge ? '35%' : '40%';
-/*   div.style.height = isLarge ? '200px' : '300px'; */
+    function toggleSize() {
+      // Change the size of the div based on its current state
+      div.style.width = isLarge ? '35%' : '40%';
+      /*   div.style.height = isLarge ? '200px' : '300px'; */
 
-  isLarge = !isLarge; // Toggle state
-}
+      isLarge = !isLarge; // Toggle state
+    }
 
-setInterval(toggleSize, 100); // Adjust the size every 1 second
+    setInterval(toggleSize, 100); // Adjust the size every 1 second
 
-//setInterval(toggleSize, 1000);
+    //setInterval(toggleSize, 1000);
 
+  }
 });
 
 function closePopup() {
-    document.getElementById("info").style.display = "none";
+  document.getElementById("info").style.display = "none";
 }
 function buttonClick(number) {
-  if (number == 1 ){
-    console.log("bruh")
+  if (number == 1) {
     document.getElementById("lButton").style.visibility = "visible";
     document.getElementById("info").style.display = "none";
 
   }
   else if (number == 2) {
     document.getElementById("rButton").style.visibility = "hidden";
-    console.log("idk")
     document.getElementById("lButton").style.visibility = "visible";
   }
 
-  
+
 }
