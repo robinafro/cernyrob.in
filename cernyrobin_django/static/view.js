b963600b91bef9526c88233b350194a5989541b6
Vítek Adam <stuckinvim@protonmail.com>
2024-01-16 22:14:44 +0100

    Improved wrapping functionality

--- cernyrobin_django/static/view.js
+++ cernyrobin_django/static/view.js
@@ -21,8 +21,10 @@
       var article = articles[i];
       if (article.style.maxHeight) {
         article.style.maxHeight = null;
+/*         article.style.margin = "0px 0px -5.5px 0px" */
       } else {
         article.style.maxHeight = article.scrollHeight + "px";
+/*         article.style.margin = "3px 3px 3px 3px" */
       }
     }
   }
