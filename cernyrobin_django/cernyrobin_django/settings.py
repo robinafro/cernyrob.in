c85d62a95c004a1bd9cf0b8104f272e8b79a6436
Vítek Adam <stuckinvim@protonmail.com>
2024-01-21 21:10:53 +0100

    Removed useless settings

--- cernyrobin_django/cernyrobin_django/settings.py
+++ cernyrobin_django/cernyrobin_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".localhost", ".cernyrob.in", "localhost", "cernyrob.in", "127.0.0.1"]
+ALLOWED_HOSTS = [".localhost", ".cernyrob.in", "localhost", "cernyrob.in"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://cernyrob.in",
