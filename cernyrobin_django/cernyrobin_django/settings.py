227bb86ad56982d269e9cdbaa4c6bebb6d698897
Vítek Adam <stuckinvim@protonmail.com>
2024-01-24 07:45:38 +0100

    Edited allowed hosts

--- cernyrobin_django/cernyrobin_django/settings.py
+++ cernyrobin_django/cernyrobin_django/settings.py
@@ -41,7 +41,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://.localhost",
     "http://.cernyrob.in",
     "http://.localhost",
-    "http://127.0.0.1"
+    "http://127.0.0.1",
+    "http://me",
 ]
 
 # Application definition
