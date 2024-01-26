f745d4ffb5344b605f1857721d17e4230108a1ae
Vítek Adam <stuckinvim@protonmail.com>
2024-01-26 19:55:26 +0100

    Set ALLOWED_HOSTS to *

--- cernyrobin_django/cernyrobin_django/settings.py
+++ cernyrobin_django/cernyrobin_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".localhost", ".cernyrob.in", "localhost", "cernyrob.in", "me", "10.1.2.6", "192.168.219.42"]
+ALLOWED_HOSTS = ["*", ".localhost", ".cernyrob.in", "localhost", "cernyrob.in", "me", "10.1.2.6", "192.168.219.42"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://cernyrob.in",
