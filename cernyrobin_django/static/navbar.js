3dbad46c95b9d42739918ae3ba9aa1001e7f4b6d
Vítek Adam <stuckinvim@protonmail.com>
2024-01-31 23:16:51 +0100

    Initialised code for collapsable sidebar menus

--- cernyrobin_django/static/navbar.js
+++ cernyrobin_django/static/navbar.js
@@ -1,3 +1,27 @@
+function changeOpenStatusSide() {
+    var articles = document.getElementById("sidebar-dropdown-container");
+    for (var i = 0; i < articles.length; i++) {
+      var article = articles[i];
+      if (article.style.maxHeight) {
+        article.style.maxHeight = null;
+        console.log("hidden")
+/*         article.style.margin = "0px 0px -5.5px 0px" */
+      } else {
+        article.style.maxHeight = article.scrollHeight + "px";
+/*         article.style.margin = "3px 3px 3px 3px" */
+console.log("showed")
+      }
+    }
+  }
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    changeOpenStatusSide()
+  });
+
+
+
+  
 document.getElementById('hamburger-menu').addEventListener('click', function() {
     console.log(document.getElementById('sidebar').style.width)
     if (document.getElementById('sidebar').style.width == "0px" || document.getElementById('sidebar').style.width == ""){
@@ -10,4 +34,8 @@ document.getElementById('hamburger-menu').addEventListener('click', function() {
         document.getElementById('sidebar').style.width = '0';
     }
     });
+
+
+
+
 
