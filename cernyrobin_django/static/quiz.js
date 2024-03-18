26eaf90b92303507ad0a83fd806f8b746dc5a09d
Vítek Adam <stuckinvim@protonmail.com>
2024-03-18 11:27:20 +0100

    Quiz page dynamic

---
+++ cernyrobin_django/static/quiz.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var quizButtonElement = document.getElementById('answer-button')
+    var titleText = document.getElementById("video-name").innerText
+    
+
+    const queryString = window.location.search
+    const queryParams = new URLSearchParams(queryString)
+    const topic = queryParams.get('topic')
+
+    let questions = [
+        'kdy vznikl unix timestamp',
+        'kdo udělal linux',
+        'kdo udělal gnu',
+    ]
+    let userAnswers = []
+
+    let currentQuestionIndex = 0
+
+    function displayQuestion() {
+        var questionElement = document.getElementById('quiz-question')
+        questionElement.textContent = questions[currentQuestionIndex]
+    }
+
+    function checkAnswer(answer) {
+        userAnswers.push(answer)
+        currentQuestionIndex++
+        if (currentQuestionIndex < questions.length) {
+            displayQuestion()
+            document.getElementById('answer-input').value = ""
+        } else {
+            console.log(userAnswers)
+            document.getElementById("question-container").style.display = "none"
+            document.getElementById("user-answer-container").style.display = "none"
+
+            document.getElementById("video-name").innerText = "Dokončil jsi " + titleText;
+
+        }
+    }
+
+    quizButtonElement.addEventListener('click', function () {
+        var answerInputElement = document.getElementById('answer-input')
+        var answer = answerInputElement.value
+        checkAnswer(answer)
+        console.log(answer)
+    })
+})
