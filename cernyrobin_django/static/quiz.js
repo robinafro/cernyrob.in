8c993f8858e9691f408b0e8391586a1b277fd9de
Vítek Adam <stuckinvim@protonmail.com>
2024-03-18 15:01:17 +0100

    Revert "moved"
    This reverts commit 582556ca5c71ed7d32be3cbbd0138ab94fa732a4.

--- cernyrobin_django/static/quiz.js
+++ cernyrobin_django/static/quiz.js
@@ -1,18 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
     var quizButtonElement = document.getElementById('answer-button')
-    var titleText = document.getElementById('video-name').innerText
+    var titleText = document.getElementById("video-name").innerText
+    
 
     const queryString = window.location.search
     const queryParams = new URLSearchParams(queryString)
     const topicSlug = queryParams.get('topic')
 
-    const questionApiPath =
-        window.location.origin + '/kafka/quiz/questions?topic=' + topicSlug
-    const topicApiPath =
-        window.location.origin + '/kafka/quiz/info?topic=' + topicSlug
+    const questionApiPath = window.location.origin + "/kafka/quiz/questions?topic=" + topicSlug
+    const topicApiPath = window.location.origin + "/kafka/quiz/info?topic=" + topicSlug
     let questions = []
-    let currentQuestionIndex = 0
+
+    fetch(topicApiPath)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+           let topic = data["course_name"]
+              document.getElementById("video-name").innerText = document.getElementById("video-name").innerText + " " + topic
+           console.log(topic)
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    fetch(questionApiPath)
+        .then(response => response.json())
+        .then(data => {
+           for (const [question, answer] of Object.entries(data.questions)) {
+                questions.push(question)
+                console.log(question)
+                document.getElementById("quiz-question").innerText = questions[0]
+              }
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+
+
+
+
     let userAnswers = []
+
+    let currentQuestionIndex = 0
 
     function displayQuestion() {
         var questionElement = document.getElementById('quiz-question')
@@ -24,49 +55,21 @@ document.addEventListener('DOMContentLoaded', function () {
         currentQuestionIndex++
         if (currentQuestionIndex < questions.length) {
             displayQuestion()
-            document.getElementById('answer-input').value = ''
+            document.getElementById('answer-input').value = ""
         } else {
             console.log(userAnswers)
-            document.getElementById('question-container').style.display = 'none'
-            document.getElementById('user-answer-container').style.display =
-                'none'
+            document.getElementById("question-container").style.display = "none"
+            document.getElementById("user-answer-container").style.display = "none"
 
-            document.getElementById('video-name').innerText =
-                'Dokončil jsi ' + titleText
+            document.getElementById("video-name").innerText = "Dokončil jsi " + titleText;
+
         }
     }
 
-    fetch(topicApiPath)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            let topic = data['course_name']
-            document.getElementById('video-name').innerText =
-                document.getElementById('video-name').innerText + ' ' + topic
-            console.log(topic)
-        })
-        .catch((error) => {
-            console.error('Error:', error)
-        })
-
-    fetch(questionApiPath)
-        .then((response) => response.json())
-        .then((data) => {
-            for (const [question, answer] of Object.entries(data.questions)) {
-                questions.push(question)
-                console.log(question)
-                document.getElementById('quiz-question').innerText =
-                    questions[0]
-                    quizButtonElement.addEventListener('click', function () {
-                        var answerInputElement = document.getElementById('answer-input')
-                        var answer = answerInputElement.value
-                        checkAnswer(answer)
-                        console.log(answer)
-                    })
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error)
-        })
-
+    quizButtonElement.addEventListener('click', function () {
+        var answerInputElement = document.getElementById('answer-input')
+        var answer = answerInputElement.value
+        checkAnswer(answer)
+        console.log(answer)
+    })
 })
