6763cdc1e8c500057817ca1a3f4dd2219a475617
Actulurus <actulurus@gmail.com>
2024-03-18 15:14:10 +0100

    fix

--- cernyrobin_django/kafka/views.py
+++ cernyrobin_django/kafka/views.py
@@ -392,7 +392,7 @@ def quiz_get_questions(request):
 
 def quiz_evaluate(request):
     questions_answers = request.POST.get("questions_answers")
-    console.log(questions_answers)
+    print(questions_answers)
     id = request.POST.get("topic")
 
     course = quiz.get_courses(id=id)
